33688356eae75364ad27f21cbf58c91515742c80
Keith Isham <kisham8899@gmail.com>
2020-01-10 12:11:47 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
+<<<<<<< HEAD
   validates :email, uniqueness: true
+=======
+    validates :email, uniqueness: true, length: { minimum: 2}
+>>>>>>> 0211cc36776688e84d5a33b31b2c5e8c2632cfc6
 end
